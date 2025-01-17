427d5c444b0df84745b18c5ad46f748347b9956c
Nikki <nikkirosecarlos@gmail.com>
2025-01-17 14:52:08 +0900

    add django method to get random secret key

--- pet_sitter_pro/settings.py
+++ pet_sitter_pro/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from environs import Env
 import socket
+from django.core.management.utils import get_random_secret_key
 
 env = Env()
 env.read_env()
@@ -12,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
