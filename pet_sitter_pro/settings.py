4ea1538e87258016d65581fde650d45d71d90a64
Nikki <nikkirosecarlos@gmail.com>
2025-01-17 14:39:07 +0900

    fix deployment settings

--- pet_sitter_pro/settings.py
+++ pet_sitter_pro/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
